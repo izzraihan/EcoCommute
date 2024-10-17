53b1312f7a17bd984836acf2d18c3cffe37dc85c
Izaz Raihan <64541012+izzraihan@users.noreply.github.com>
2024-10-17 20:25:28 +0700

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -3,6 +3,8 @@ $host = 'localhost';
 $db = 'tukarpoin_db';
 $user = 'binus_fadli';  // Ganti dengan user MySQL yang Anda buat
 $pass = 'asdasdasd';  // Ganti dengan password MySQL yang sesuai
+$user = 'binus_izaz';  // Ganti dengan user MySQL yang Anda buat
+$pass = 'P@ssw0rd';  // Ganti dengan password MySQL yang sesuai
 
 $conn = new mysqli($host, $user, $pass, $db);
 
